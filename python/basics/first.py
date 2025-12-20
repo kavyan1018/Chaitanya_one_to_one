845b73249ea761d3827209675a2744c853f0100f
Kavyan Nayak <kavyannayak104@gmail.com>
2025-12-20 13:28:47 +0530

    display

---
+++ python/basics/first.py
@@ -0,0 +1,27 @@
+# single line comment
+'''
+    This is a multi-line comment
+'''
+
+print("Hello World!")   
+
+# variables
+# int , float, string, boolean, char  -> c 
+
+x = 100    #int 
+
+print("Value of x is:", x)
+print(x)
+
+per = 99.9   # float
+print("Percentage is:", per)
+
+
+user = "ram"
+print("User is:", user)
+
+# void 
+
+data = None
+print("Data is:", None)
+print("Type of data :", type(None))
