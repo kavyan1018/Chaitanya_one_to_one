b25fea6174dbcea55e09b8f35a6d8a5958ee0728
Kavyan Nayak <kavyannayak104@gmail.com>
2026-01-03 13:16:42 +0530

    elis

---
+++ python/if-else/elif.py
@@ -0,0 +1,22 @@
+"""
+Docstring for python.if-else.elif
+
+if condition1 :
+    # code to be executed if condition is true
+
+elif condition2 :
+    # code to be executed if condition2 is true
+
+else :
+    # code to be executed if both conditions are false
+
+"""
+
+a = int(input("Enter first number: "))
+
+if a > 0 :
+    print("The number is positive")
+elif a == 0 :
+    print("The number is zero")
+else :
+    print("The number is negative")
