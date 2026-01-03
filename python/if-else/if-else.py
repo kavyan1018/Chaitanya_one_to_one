ed78cba417f17a28235e68ab7154ec53473eef6f
Kavyan Nayak <kavyannayak104@gmail.com>
2026-01-03 13:17:26 +0530

    if

---
+++ python/if-else/if-else.py
@@ -0,0 +1,31 @@
+'''
+if()
+{
+    
+}else
+{
+    
+}
+
+if condition:
+    Code pass
+else:
+    Code pass
+
+'''
+
+# the numebr is positive or negative
+
+# a = -10
+
+# if a >= 0:
+#     print("The number is positive")
+# else:
+#     print("The number is negative")
+
+
+a = int(input("Enter a number: "))
+if a >= 0:
+    print("The number is positive")
+else:
+    print("The number is negative")
