afc190dc6c2312d80cb69284d7382da32f7bc258
Kavyan Nayak <kavyannayak104@gmail.com>
2026-02-04 13:35:50 +0530

    loops

---
+++ python/loops/loops.py
@@ -0,0 +1,29 @@
+'''
+
+    types of loops in Python
+        for loop :
+            
+            for variable in sequence :
+                print(variable)
+            
+        while loop :
+            while condition :
+                print(variable)
+                i += 1
+
+'''
+i = 1
+
+# for loop
+print("for loop")
+
+for i in range(0, 7):
+    print(i)
+    
+# while loop
+
+i = 1
+print("while loop")
+while i < 7:
+    print(i)
+    i += 1
