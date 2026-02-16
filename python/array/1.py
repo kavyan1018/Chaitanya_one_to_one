246f97c598973743054485707d8135704df6d2e1
Kavyan Nayak <kavyannayak104@gmail.com>
2026-02-16 11:23:20 +0530

    Array

---
+++ python/array/1.py
@@ -0,0 +1,13 @@
+# Creating an array 
+
+arr = [1, 2, 3, 4, 5]
+
+# Accessing elements of an array -> each element 
+# print(arr[0])
+# print(arr[3])
+
+
+# modifying an element of an array
+arr[3] = 40
+
+print(arr)
